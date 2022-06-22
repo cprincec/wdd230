9e4d006a5d98fa285d773be2b21cdefb7dcae222
cprincec <chukwuprincec@gmail.com>
2022-06-22 11:53:03 +0100

    date

--- week2/weather/scripts/togglemenu.js
+++ week2/weather/scripts/togglemenu.js
@@ -1,5 +1,8 @@
 function toggleMenu()   {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-    
-    
-}
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");    
+};
+
+
+const date = new Date(document.lastModified).toString();
+const updated = document.querySelector("#updated");
+updated.textContent = `Last Updated: ${date}`;
